53d7a753624115498a04d05e84ebc8e9112deb49
raghups <raghu.bharadwaj35@gmail.com>
2020-06-25 12:04:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
     response.end("Hello Australia335566!");
+    response.end("Hello World!");
+    
 });
 
 const port = process.env.PORT || 1337;
